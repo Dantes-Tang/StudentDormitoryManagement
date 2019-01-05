c18eb8430a9dbf9fe3c9eda128fc85d5a61fa69d
changgu <changgu@meili-inc.com>
2019-01-05 16:43:27 +0800

    init

---
+++ src/main/java/com/dantes/StudentDormitoryManagement/StudentDormitoryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.dantes.StudentDormitoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDormitoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDormitoryManagementApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dantes/StudentDormitoryManagement/StudentDormitoryManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dantes.StudentDormitoryManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentDormitoryManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
